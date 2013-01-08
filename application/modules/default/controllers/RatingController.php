f1619f837d996bf0fbe4071da98da62ccbd6ec40
vlado <v.vidinliev@gmail.com>
2013-01-08 21:31:35 +0200

    test
    Signed-off-by: vlado <v.vidinliev@gmail.com>

--- application/modules/default/controllers/RatingController.php
+++ application/modules/default/controllers/RatingController.php
@@ -3,100 +3,99 @@
 /**
  * 
  */
-class Default_RatingController extends Zend_Controller_Action
-{
-    
+class Default_RatingController extends Zend_Controller_Action {
+
     /**
      *
      * @var type
      */
     protected $_model;
 
-    public function init()
-    {
-       $this->_helper->authentication->checkAuthentication(false);
-       $this->_model = new Model_Document();
+    public function init() {
+        $this->_helper->authentication->checkAuthentication(false);
+        $this->_model = new Model_Document();
     }
 
-    public function indexAction()
-    {
+    public function indexAction() {
         $user = new Model_User();
         $user = $user->getUser(array('id' => Service_Auth::getLoggedUser()->getId()));
-        
+
+        $config = Zend_Registry::get('config');
+
         //document type id
         $doctype = new Model_DocumentType();
         $doctype = $doctype->get(1);
         $paginator = new Zend_Paginator(
-                        new App_Paginator_Adapter_Doctrine($this->_model->getAll(array('user' => $user,'documentType'=>$doctype))));
+                        new App_Paginator_Adapter_Doctrine($this->_model->getAll(array('user' => $user, 'documentType' => $doctype))));
 
         $paginator->setCurrentPageNumber($this->_getParam('page'));
 
         $paginator->setItemCountPerPage(Model_Abstract::PER_PAGE);
         $this->view->documents = $paginator;
-       
-    }  
-    
-    public function addressAction()
-    {
+        $this->view->imagePath = $config->paths->validate_files;
+    }
+
+    public function addressAction() {
         $user = new Model_User();
         $user = $user->getUser(array('id' => Service_Auth::getLoggedUser()->getId()));
-        
+
+        $config = Zend_Registry::get('config');
+
         //document type address
         $doctype = new Model_DocumentType();
         $doctype = $doctype->get(2);
         $paginator = new Zend_Paginator(
-                        new App_Paginator_Adapter_Doctrine($this->_model->getAll(array('user' => $user,'documentType'=>$doctype))));
+                        new App_Paginator_Adapter_Doctrine($this->_model->getAll(array('user' => $user, 'documentType' => $doctype))));
 
         $paginator->setCurrentPageNumber($this->_getParam('page'));
 
         $paginator->setItemCountPerPage(Model_Abstract::PER_PAGE);
         $this->view->documents = $paginator;
-       
+        $this->view->imagePath = $config->paths->validate_files;
     }
-    
-     public function addressUploadAction() 
-    {
+
+    public function addressUploadAction() {
         $id = Service_Auth::getLoggedUser()->getId();
-       
+
         $form = new Default_Form_Address();
-        
+
         $document = $this->_model->get($id);
-       
-       
+
+
 
         if ($this->_request->isPost()) {
-            
+
             $post = $this->_request->getPost();
-            
+
             if ($form->isValid($post)) {
                 $document = $this->_model->create($form->getValues(), $id);
-                
+
                 if ($document) {
-                    $this->_helper->redirector('index');
+                    $this->_helper->redirector('address');
                 }
             }
         }
 
         $this->view->assign(array('form' => $form, 'id' => $id));
     }
-    
-    public function idUploadAction(){
-        
+
+    public function idUploadAction() {
+
         $id = Service_Auth::getLoggedUser()->getId();
-       
+
         $form = new Default_Form_Id();
-        
+
         $document = $this->_model->get($id);
-       
-       
+
+
 
         if ($this->_request->isPost()) {
-            
+
             $post = $this->_request->getPost();
-            
+
             if ($form->isValid($post)) {
                 $document = $this->_model->create($form->getValues(), $id);
-                
+
                 if ($document) {
                     $this->_helper->redirector('index');
                 }
@@ -104,6 +103,6 @@ class Default_RatingController extends Zend_Controller_Action
         }
 
         $this->view->assign(array('form' => $form, 'id' => $id));
-        
     }
+
 }
