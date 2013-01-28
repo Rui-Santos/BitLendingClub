40f80851db4830ddf35af1b2ede6aed383fe3036
Vladimir Vidinliev <v.vidinliev@gmail.com>
2013-01-29 01:30:30 +0200

    dashboard complete
    Signed-off-by: Vladimir Vidinliev <v.vidinliev@gmail.com>

--- application/modules/default/controllers/ProfileController.php
+++ application/modules/default/controllers/ProfileController.php
@@ -20,24 +20,42 @@ class Default_ProfileController extends Zend_Controller_Action {
         $id = Service_Auth::getLoggedUser()->getId();
         $user = $this->_model->get($id);
         $this->view->user = $user;
-        
+
+        $validated = 0;
+        $validatedAddress = 0;
+        $validatedId = 0;
         $wallet = new Model_Wallet();
         $wallets = $wallet->findBy(array('user' => $user->getId()));
         $this->view->wallets = $wallets;
-        
+
         $loans = new Model_Loan();
         $loans = $loans->findBy(array('borrower' => $user->getId()));
         $this->view->loans = $loans;
-        
+
         $investments = new Model_Investment();
         $investments = $investments->findBy(array('investor' => $user->getId()));
         $this->view->investments = $investments;
-        
+
         $documents = new Model_Document();
         $doctype = new Model_DocumentType();
         $doctypeId = $doctype->get(1);
-        $docId = $documents->findBy(array('user'=>$id,'isReviewed'=>1));
-        App_DoctrineDebug::dump($docId);exit;
+        $docs = $documents->findBy(array('user' => $id, 'isReviewed' => 1));
+        if (count($docs) > 0):
+            foreach ($docs as $doc):
+                $validated++;
+                if ($doc->getDocumentType() === $doctypeId):
+                    $validatedId = 1;
+                else:
+                    $validatedAddress = 1;
+                endif;
+            endforeach;
+        endif;
+        if($user->getFbUserId()):
+            $validated++;
+        endif;
+        $this->view->validated = $validated;
+        $this->view->valId = $validatedId;
+        $this->view->valAddress = $validatedAddress;
         //Zend_Debug::dump(Zend_Auth::getInstance()->getIdentity());
     }
 
@@ -84,11 +102,11 @@ class Default_ProfileController extends Zend_Controller_Action {
     }
 
     public function investmentsAction() {
-         $id = Service_Auth::getLoggedUser()->getId();
+        $id = Service_Auth::getLoggedUser()->getId();
         $userItem = $this->_model->get($id);
 
         $loanModel = new Model_Investment();
-        
+
         $paginator = new Zend_Paginator(
                         new App_Paginator_Adapter_Doctrine($loanModel->getAll(array('investor' => $userItem))));
 
@@ -103,7 +121,7 @@ class Default_ProfileController extends Zend_Controller_Action {
         $userItem = $this->_model->get($id);
 
         $loanModel = new Model_Loan();
-        
+
         $paginator = new Zend_Paginator(
                         new App_Paginator_Adapter_Doctrine($loanModel->getAll(array('borrower' => $userItem))));
 
