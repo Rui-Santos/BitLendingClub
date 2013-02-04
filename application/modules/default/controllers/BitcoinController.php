d0e0d42a1d3caf60adad9b708849640f22f1bb29
Vladimir Vidinliev <v.vidinliev@gmail.com>
2013-02-04 23:07:08 +0200

    testing bitcoin api
    Signed-off-by: Vladimir Vidinliev <v.vidinliev@gmail.com>

---
+++ application/modules/default/controllers/BitcoinController.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * 
+ */
+class Default_BitcoinController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+      
+    }
+
+    public function indexAction()
+    {
+        
+       // Zend_Debug::dump(Zend_Auth::getInstance()->getIdentity());
+       
+    }  
+    
+}
